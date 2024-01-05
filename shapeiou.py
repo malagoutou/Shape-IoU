6cc086ef3960783e0d782e3bdd77ce5ecd14bb02
malagoutou <150572966+malagoutou@users.noreply.github.com>
2024-01-05 21:41:27 +0800

    Update shapeiou.py

--- shapeiou.py
+++ shapeiou.py
@@ -1,4 +1,4 @@
-def shape_iou(box1, box2, xywh=True, scale=0  eps=1e-7):
+def shape_iou(box1, box2, xywh=True, scale=0, eps=1e-7):
     (x1, y1, w1, h1), (x2, y2, w2, h2) = box1.chunk(4, -1), box2.chunk(4, -1)
     w1_, h1_, w2_, h2_ = w1 / 2, h1 / 2, w2 / 2, h2 / 2
     b1_x1, b1_x2, b1_y1, b1_y2 = x1 - w1_, x1 + w1_, y1 - h1_, y1 + h1_
